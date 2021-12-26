64403cab9c6645533ede9f3a3cab1b22d80e5fcf
Tan Wei Kang <tanweikang02@gmail.com>
2021-12-26 16:20:01 +0800

    complete doubly_linked_list

--- doubly_linked_list.py
+++ doubly_linked_list.py
@@ -5,7 +5,7 @@ class Node:
         self.prev = prev
 
 
-class DoubleLinkedList:
+class DoublyLinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
@@ -23,3 +23,39 @@ class DoubleLinkedList:
             self.tail = node
 
         self.size += 1
+
+    def delete(self, data):
+        curr = self.head
+        while curr != None:
+            if curr.data == data:
+                if curr == self.head:
+                    self.head = curr.next
+                    curr.next.prev = None
+                elif curr == self.tail:
+                    self.tail = curr.prev
+                    curr.prev.next = None
+                else:
+                    curr.prev.next = curr.next
+                    curr.next.prev = curr.prev
+
+                self.size -= 1
+                return
+
+            curr = curr.next
+
+
+linked_list = DoublyLinkedList()
+linked_list.append("Item 1")
+linked_list.append("Item 2")
+linked_list.append("Item 3")
+linked_list.append("Item 4")
+linked_list.append("Item 5")
+linked_list.append("Item 6")
+linked_list.delete("Item 5")
+linked_list.delete("Item 1")
+
+# Test cases
+
+assert linked_list.head.data == "Item 2"
+assert linked_list.tail.data == "Item 6"
+assert linked_list.size == 4
